107c618ba7e4855acc376f07dc3b7da16639f309
federicaarosio <arosio.fede@gmail.com>
2023-12-28 17:43:52 +0100

    es. 15 - completato

--- esercizi/esercizio_15.js
+++ esercizi/esercizio_15.js
@@ -5,7 +5,7 @@
 
 const numbers = [10, 43, -34, -2, 78];
 let average = 0;
-for (let i = _; i < _; _) {
-    _
+for (let i = 0; i < numbers.length; i++) {
+    average + numbers[i];
 }
 console.log('Esercizio 15', average / numbers.length);
